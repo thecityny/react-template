2d955649b94a1a4edb9d586afd1859ab19713794
Sam Rabiyah <sraby@users.noreply.github.com>
2022-11-01 22:16:30 -0400

    Another attempt to fix svg loading problem

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+/** @type {import('next').NextConfig} */
 
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find(
+      (rule) => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: require.resolve("@svgr/webpack"),
+    });
     return config;
   },
 };
