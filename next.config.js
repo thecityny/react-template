3b6e51c623014976d37081c07020f2df050bce32
Sam Rabiyah <sraby@users.noreply.github.com>
2022-11-01 23:05:52 -0400

    Add back config options

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  assetPrefix: ".",
+};
+
+module.exports = nextConfig;
+
 // next.config.js
 const withSvgr = require("@newhighsco/next-plugin-svgr");
 module.exports = withSvgr();
